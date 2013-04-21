c8ea7e1ec142ec69e1cb1b10573b1f45fe233f2e
andreimunteanu <mantundrei@gmail.com>
2013-04-21 12:02:10 +0300

    Update revert.sh

--- revert.sh
+++ revert.sh
@@ -1,52 +1,51 @@
 #!/bin/bash
 if [ $# -eq 0 ] || [ $# -gt 2 ]
 	then
-	echo "Errore: utilizzo: ciao...  "
-	exit;
+		echo "Errore: utilizzo: ciao...  "
+		exit;
 
 else
 	if [ ! -d ./$1 ]
 		then
-		echo "cartella non esistente"
-		exit;
+			echo "<repo> "$1" non esistente"
+			exit;
 	fi
 	
 
 	if [ $# -eq 1 ]
 		then	
-		cd ".$@.bck"
-		cp -rf . ../$@ 
-		cd ..;
+			cd ".$@.bck"
+			cp -rf . ../$@ 
+			cd ..;
 	
 	elif [ $# -eq 2 ]
 		then	
 			cd ".$1.bck"
-			var=$(find . -name $2)
-			arrIN=(${var// / })
-			n=${#arrIN[@]}
-			echo $n
+			files_list=$(find . -name $2)
+			files_list=($files_list) #(${var//" "/" \n"})
+			n=${#files_list[@]} # grep che conta quanti file ha trovato
 			if [ $n -eq 0 ]
 				then
-				echo "file "$2" non trovato"
-				exit;
+					echo "file "$2" non trovato"
+					exit;
 	
 			elif [ $n -eq 1 ]		# caso singolo file trovato
 				then
-				echo ciao
-				rsync -r $var ../$1 
+					echo ciao
+					rsync -r $files_list ../$1 
 			else
-				var=""
-				for el in ${arrIN[@]}; do
-					var+=$el"\n"
+				files=""
+				for el in ${files_list[@]}; do
+					files+=$el"\n"
 				done
-				echo -e $var
+				echo -e $files
 				echo "A causa di multiplo ritrovamento Inserisci nome directory o invio per la dir corrente"
 				read dir_name
 				if [ -z $dir_name ]
 					then
 						cp -rf $2 ../$1; # pensa prima di sparare
 				else
-					rsync -R $(echo -e $var | grep -w "\<$dir_name\>") ../$1/;
+					rsync -R $(echo -e $files | grep -w "\<$dir_name\>") ../$1/;
 				fi
 				cd ..
 			fi
