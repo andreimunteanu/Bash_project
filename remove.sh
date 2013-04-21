a327a1ca622f0931e4b1d6ad95ace83bdd5b1495
Andrei Munteanu <mantundrei@gmail.com>
2013-04-21 11:36:54 +0200

    one line

--- remove.sh
+++ remove.sh
@@ -5,7 +5,6 @@ if [ $# -ne 1 ]
 		exit;
 fi
 
-<<<<<<< HEAD
 file_list=$(find . -name $@)
 file_list=${file_list//"./"/"\n./"}
 
@@ -18,25 +17,6 @@ if [ $n -eq 2 ] || [ $n -eq 1 ]		# caso singolo file trovato
 
 else
 	echo -e $file_list | grep -v "bck"
-=======
-file_list=($(find . -name $@))
-
-n=${#file_list[@]} 
-
-if [ $n -eq 2 ] || [ $n -eq 1 ]		# caso singolo file trovato
-	then
-		rm -f $file_list;
-
-else
-	files=""	
-	for el in ${file_list[@]}; do     
-		files+=$el"\n"
-		if [ -z $(echo $el | grep 'bck') ]
-			then
-				echo $el;
-		fi
-	done
->>>>>>> c8ea7e1ec142ec69e1cb1b10573b1f45fe233f2e
 	echo "A causa di multiplo ritrovamento Inserisci nome repo"
 	
 	read name
@@ -45,11 +25,6 @@ else
 			echo  "<repo> "$name" non esistente "
 			exit;
 	fi
-<<<<<<< HEAD
 	
 	rm -f $(echo -e $files | grep -w "$name") # da controllare la rindondanza di -w e \< \>
-=======
-
-	rm -f $(echo -e $files | grep -w "\<$name\>") # da controllare la rindondanza di -w e \< \>
->>>>>>> c8ea7e1ec142ec69e1cb1b10573b1f45fe233f2e
 fi
