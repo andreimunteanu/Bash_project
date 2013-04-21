5fe50aebc84b313087d72bdd5ffe4d19b44642a8
Andrei Munteanu <mantundrei@gmail.com>
2013-04-21 12:50:40 +0200

    comments

--- remove.sh
+++ remove.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-if [ $# -ne 1 ]
-	then 
-		echo "utilizzo errato: remove <file_name>"
-		exit;
+if [ $# -ne 1 ]                         # controlla numero di parametri
+then 
+    echo "utilizzo errato: remove <file_name>"
+    exit;                               # se diverso da 1, esci
 fi
+                                        # cerca tutti i file file_name nella directory
+file_list=$(find . -name $@)            # corrente e nelle sue sotto directory 
+file_list=${file_list//"./"/"\n./"}     # salva i risultati per righe distinte
 
-file_list=$(find . -name $@)
-file_list=${file_list//"./"/"\n./"}
-
-n=$(echo -e $file_list | grep -wc "$@")
+n=$(echo -e $file_list | grep -wc "$@") # salva il numero di risultati
 
 if [ $n -eq 2 ] || [ $n -eq 1 ]		# caso singolo file trovato
-	then
-		echo ciao grazie
-		rm -f $file_list;
-
-else
-	echo -e $file_list | grep -v "bck"
-	echo "A causa di multiplo ritrovamento Inserisci nome repo"
-	
-	read name
-	if [ ! -d ./$name ]
-		then
-			echo  "<repo> "$name" non esistente "
-			exit;
-	fi
-	
-	rm -f $(echo -e $files | grep -w "$name") # da controllare la rindondanza di -w e \< \>
+then                                    # in una sola repo con possibile
+    echo ciao grazie                    # copia di backup
+    rm -f $file_list;                   # rimuove il file e relativa copia(se esiste)
+    
+else                                    # se più repo contengono il file file_name
+    echo -e $file_list | grep -v "bck"                  # stampa a video le righe delle repo che 
+    echo "Più di una repository contiene il file "$1"." # contegono il file senza le relative copie di backup
+    echo "Inserisci nome repository."
+                                        # attende in input il nome della repo
+    read name                           # o una sua sotto-directory
+    if [ ! -d ./$name ]                 # se la directory name non esiste
+    then                                # esce
+	echo  $name" non esistente"
+	exit;
+    fi
+    
+    rm -f $(echo -e $files | grep -w "$name") #rimuove i/il file dalla directory name
 fi
--- revert.sh
+++ revert.sh
@@ -1,52 +1,50 @@
 #!/bin/bash
-if [ $# -eq 0 ] || [ $# -gt 2 ]
-	then
-		echo "Errore: utilizzo: ciao...  "
-		exit;
-
+if [ $# -eq 0 ] || [ $# -gt 2 ]              # controlla il numero di parametri passati
+then
+    echo "Errore, utilizzo: <repo> [file]  " # esci se non corrsiponde
+    exit;
+		
 else
-	if [ ! -d ./$1 ]
-		then
-			echo "<repo> "$1" non esistente"
-			exit;
+    if [ ! -d ./$1 ]                      # controlla se la repo esiste
+    then
+	echo "<repo> "$1" non esistente." # esce se non esiste
+	exit;
+    fi
+    
+    if [ $# -eq 1 ]    # caso: un solo parametro 
+    then	       # copia tutto da .<repo>.bck in <repo> 
+	cd ".$@.bck" 
+	cp -rf . ../$@ 
+	cd ..;
+	
+    elif [ $# -eq 2 ]                          # caso: due parametri
+    then	
+	cd ".$1.bck"                           # cambia directory di lavoro: .<repo>.bck
+	file_list=$(find . -name $2)           # cerca tutti i <file> in .<rep>.bck
+	file_list=${file_list//"./"/"\n./"}    # salva i risultati su righe distinte 
+	                                       # nella variabile file_list
+	n=$(echo -e $file_list | grep -wc "$2")# salva quanti <file> sono stati trovati  
+                                               # in .<repo>.bck	
+	if [ $n -eq 0 ]                        # se non sono stati trovati file: esci
+	then
+	    echo "file "$2" non trovato."
+	    exit;
+	    
+	elif [ $n -eq 1 ]		       # caso singolo file trovato
+	then                                   # copia il file da .<repo>.bck in <repo>
+	    rsync -R $(echo -e $file_list) ../$1 
+	else                                   # se ci sono più file
+	    echo -e $file_list
+	    echo "In "$1"c'è più di un file di nome "$2"."
+	    echo "Inserisci nome directory o invio per la dir corrente."
+	    read dir_name                     # prende in input da utente il nome della directory
+	    if [ -z $dir_name ]               # da cui copiare il file: <invio> per la directory corrente
+	    then
+		cp -rf $2 ../$1;              # copia dalla directory corrente
+	    else
+		rsync -R $(echo -e $file_list | grep -w "$dir_name") ../$1; # copia dalle directory dir_name il file
+	    fi                                                              # rsync prende in input solo le righe che contengono dir_name
+	    cd ..                             # esce da .<repo>.bck
 	fi
-	
-
-	if [ $# -eq 1 ]
-		then	
-			cd ".$@.bck"
-			cp -rf . ../$@ 
-			cd ..;
-	
-	elif [ $# -eq 2 ]
-		then	
-			cd ".$1.bck"
-			file_list=$(find . -name $2)
-			file_list=${file_list//"./"/"\n./"}
-			echo -e $file_list
-
-			n=$(echo -e $file_list | grep -wc "$2")
-			
-			if [ $n -eq 0 ]
-				then
-					echo "file "$2" non trovato"
-					exit;
-	
-			elif [ $n -eq 1 ]		# caso singolo file trovato
-				then
-					echo ciao
-					rsync -R $(echo -e $file_list) ../$1 #forse non funziona
-			else
-				echo -e $file_list
-				echo "A causa di multiplo ritrovamento Inserisci nome directory o invio per la dir corrente"
-				read dir_name
-				if [ -z $dir_name ]
-					then
-						cp -rf $2 ../$1; 
-				else
-					rsync -R $(echo -e $file_list | grep -w "$dir_name") ../$1;
-				fi
-			cd ..
-			fi
-		fi
+    fi
 fi
